43aa9ae30a70b8e0fee41468c80631ed9d251d49
Ulminia <ulminia@gmail.com>
2014-09-07 22:34:17 -0300

    massive local updates
    updated api item to not use hard coded text

--- lib/api/apiitem.php
+++ lib/api/apiitem.php
@@ -20,131 +20,6 @@ this file will process the json data for items and make a tooltip for us ..
 
 class ApiItem {
 
-var $itemclass = array(
-'0' => 'Consumable','1' => 'Container','2' => 'Weapon','3' => 'Gem','4' => 'Armor','5' => 'Reagent','6' => 'Projectile',
-'7' => 'Trade Goods','8' => 'Generic','9' => 'Book','10' => 'Money','11' => 'Quiver','12' => 'Quest','13' => 'Key','14' => 'Permanent',
-'15' => 'Junk','16' => 'Glyph');
-
-//itemSubClass
-
-//'classid' => 'subclassid' => 'subclassname' => 'subclassfullname',
-var $itemSubClass = array(
-'0' => array('0' => 'Consumable','5' => 'Food & Drink','1' => 'Potion','2' => 'Elixir','3' => 'Flask','7' => 'Bandage','6' => 'Item Enhancement',
-'4' => 'Scroll','8' => 'Other'),
-
-'1' => array('0' => 'Bag','1' => 'Soul Bag','2' => 'Herb Bag','3' => 'Enchanting Bag','4' => 'Engineering Bag','5' => 'Gem Bag','6' => 'Mining Bag',
-'7' => 'Leatherworking Bag','8' => 'Inscription Bag','9' => 'Tackle Box'),
-
-'2' => array('0' => 'Axe','1' => 'Axe','2' => 'Bow','3' => 'Gun','4' => 'One-Handed Mace','5' => 'Two-Handed Mace','6' => 'Polearm','7' => 'One-Handed Sword',
-'8' => 'Two-Handed Sword','9' => 'Obsolete','10' => 'Stave','11' => 'One-Handed Exotic','12' => 'Two-Handed Exotic','13' => 'Fist Weapon','14' => 'Miscellaneous',
-'15' => 'Dagger','16' => 'Thrown','17' => 'Spear','18' => 'Crossbow','19' => 'Wand','20' => 'Fishing Pole'),
-'3' => array('0' => 'Red','1' => 'Blue','2' => 'Yellow','3' => 'Purple','4' => 'Green','5' => 'Orange','6' => 'Meta','7' => 'Simple','8' => 'Prismatic',
-'9' => 'Hydraulic','10' => 'Cogwheel'),
-'4' => array('0' => 'Miscellaneous','1' => 'Cloth','2' => 'Leather','3' => 'Mail','4' => 'Plate','5' => 'Bucklers','6' => 'Shield','7' => 'Libram',
-'8' => 'Idol','9' => 'Totem','10' => 'Sigil','11' => 'Relic'),
-'5' => array('0' => 'Reagent'),
-'6' => array('0' => 'Wand(OBSOLETE)','1' => 'Bolt(OBSOLETE)','2' => 'Arrow','3' => 'Bullet','4' => 'Thrown(OBSOLETE)'),
-'7' => array('0' => 'Trade Goods','10' => 'Elemental','15' => 'Weapon Enchantment - Obsolete','5' => 'Cloth','6' => 'Leather','7' => 'Metal & Stone','8' => 'Meat',
-'9' => 'Herb','12' => 'Enchanting','4' => 'Jewelcrafting','1' => 'Parts','3' => 'Devices','2' => 'Explosives','13' => 'Materials','11' => 'Other','14' => 'Item Enchantment'),
-//'0' => 'Generic(OBSOLETE)',
-'9' => array('0' => 'Book','1' => 'Leatherworking','2' => 'Tailoring','3' => 'Engineering','4' => 'Blacksmithing','5' => 'Cooking','6' => 'Alchemy','7' => 'First Aid',
-'8' => 'Enchanting','9' => 'Fishing','10' => 'Jewelcrafting','11' => 'Inscription'),
-//'0' => 'Money(OBSOLETE)',
-'11' => array('0' => 'Quiver(OBSOLETE)','1' => 'Quiver(OBSOLETE)','2' => 'Quiver','3' => 'Ammo Pouch'),
-'12' => array('0' => 'Quest'),
-'13' => array('0' => 'Key','1' => 'Lockpick'),
-'14' => array('0' => 'Permanent'),
-'15' => array('0' => 'Junk','1' => 'Reagent','2' => 'Pet','3' => 'Holiday','4' => 'Other','5' => 'Mount'),
-'16' => array('1' => 'Warrior','2' => 'Paladin','3' => 'Hunter','4' => 'Rogue','5' => 'Priest','6' => 'Death Knight','7' => 'Shaman','8' => 'Mage',
-'9' => 'Warlock','11' => 'Druid'));
-
-var $slotType = array('0' => 'None','1' => 'Head','2' => 'Neck','3' => 'Shoulder','4' => 'Shirt','5' => 'Chest','6' => 'Waist','7' => 'Legs','8' => 'Feet',
-'9' => 'Wrist','10' => 'Hands','11' => 'Finger','12' => 'Trinket','13' => 'One-Hand','14' => 'Shield','15' => 'Ranged','16' => 'Cloak','17' => 'Two-Hand',
-'18' => 'Bag','19' => 'Tabard','20' => 'Robe','21' => 'Main Hand','22' => 'Off Hand','23' => 'Held In Off-hand','24' => 'Ammo','25' => 'Thrown','26' => 'Ranged Right',
-'28' => 'Relic');
-
-
-/*
-itemQuality
-'qualityid' => 'qualityname' => 'qualitycolor',
-'0' => 'Poor' => '9D9D9D',
-'1' => 'Common' => 'FFFFFF',
-'2' => 'Uncommon' => '1EFF00',
-'3' => 'Rare' => '0070DD',
-'4' => 'Epic' => 'A335EE',
-'5' => 'Legendary' => 'FF8000',
-'6' => 'Artifact' => 'E5CC80',
-'7' => 'Heirloom' => 'E5CC80',
-'8' => 'Quality 8' => 'FFFF98',
-'9' => 'Quality 9' => '71D5FF"
-*
-stat types
-*/
-var $statlocal = array(
-'1' => 'Health',
-'2' => 'Mana',
-'3' => 'Agility',
-'4' => 'Strength',
-'5' => 'Intellect',
-'6' => 'Spirit',
-'7' => 'Stamina',
-);
-var $itemstat = array(
-'1' => '+%s Health',
-'2' => '+%s Mana',
-'3' => '+%s Agility',
-'4' => '+%s Strength',
-'5' => '+%s Intellect',
-'6' => '+%s Spirit',
-'7' => '+%s Stamina',
-'46' => 'Equip: Restores %s health per 5 sec.',
-'44' => 'Armor Penetration',
-'38' => 'Attack Power by %s.',
-'15' => 'Shield Block',
-'48' => 'the block value of your shield by %s.',
-'19' => 'Melee Critical strike',
-'20' => 'Ranged Critical strike',
-'32' => 'Critical Strike',
-'21' => '+%s Spell Critical strike',
-'25' => 'Melee Critical avoidance',
-'26' => 'Ranged Critical avoidance',
-'34' => 'Critical Avoidance',
-'27' => '+%s Spell critical avoidance',
-//ITEM_MOD_DAMAGE_PER_SECOND_SHORT => 'Damage Per Second',
-'12' => '+%s Defense',
-'13' => '+%s Dodge',
-'37' => '+%s Expertise',
-'40' => 'attack power by %s in Cat, Bear, Dire Bear, and Moonkin forms only.',
-'28' => '+%s Melee Haste',
-'29' => '+%s Ranged Haste',
-'36' => '+%s Haste',
-'30' => '+%s Spell Haste',
-'16' => '+%s Melee Hit',
-'17' => '+%s Ranged Hit',
-'31' => '+%s Hit',
-'18' => '+%s Spell Hit',
-'22' => '+%s Melee Hit Avoidance',
-'23' => '+%s Ranged Hit Avoidance',
-'33' => '+%s Hit Avoidance',
-'24' => '+%s Spell Hit Avoidance',
-'43' => 'Equip: Restores %s mana per 5 sec.',
-'49' => '+%s Mastery',
-'14' => '+%s Parry',
-'39' => '+%s Ranged Attack Power',
-'35' => '+%s PvP Resilience',
-'57' => '+%s PvP Power',
-'41' => 'damage done by magical spells and effects by up to %s.',
-'42' => 'healing done by magical spells and effects by up to %s.',
-'47' => '+%s Spell Penetration',
-'45' => '+%s Spell Power');
-
-var $bind = array(
-'0' => '','8' => "Account Bound",'2' => "Binds when equipped",'1' => "Binds when picked up",'3' => "Binds when used",'4' => "Quest Item",
-'5' => "Binds to account",'6' => "Binds to Battle.net account",'7' => "Battle.net Account Bound");
-var $skills = array(
-"185" => "Cooking","773" => "Inscription","755" => "Jewelcrafting","393" => "Skinning","333" => "Enchanting","202" => "Engineering",
-"197" => "Tailoring","186" => "Mining","182" => "Herbalism","171" => "Alchemy","165" => "Leatherworking","164" => "Blacksmithing");
-
 	var $member_id, $item_id, $name, $level, $icon, $color;
 	var $slot, $parent, $tooltip, $quantity, $locale;
 
@@ -273,11 +148,11 @@ var $skills = array(
 					{
 						if ($spell['trigger'] == 'ON_EQUIP' )
 						{
-							$tt['Effects']['Equip'][] = 'Equip: '.$spell['spell']['description'];
+							$tt['Effects']['Equip'][] = $roster->locale->act['tooltip_equip'].' '.$spell['spell']['description'];
 						}
 						else
 						{
-							$tt['Effects']['Use'][] = 'Use: '.$spell['spell']['description'];
+							$tt['Effects']['Use'][] = $roster->locale->act['tooltip_equip'].' '.$spell['spell']['description'];
 						}
 					}
 				}
@@ -363,13 +238,13 @@ var $skills = array(
 			
 			if ($data['maxDurability'] > 0)
 			{
-				$tt['Attributes']['Durability']['Line']= 'Durability';
+				$tt['Attributes']['Durability']['Line']= $roster->locale->act['tooltip_durability'];
 				$tt['Attributes']['Durability']['Current'] = ( isset($data['curDurability']) ? $data['curDurability'] : $data['maxDurability']);
 				$tt['Attributes']['Durability']['Max'] = $data['maxDurability'];
 			}
 			if (isset($data['allowableClasses']))
 			{
-				$tt['Attributes']['Class'] = 'Classes:';
+				$tt['Attributes']['Class'] = $roster->locale->act['tooltip_classes'];
 				foreach($data['allowableClasses'] as $id => $classes)
 				{
 					$tt['Attributes']['ClassText'][] = $roster->locale->act['id_to_class'][$classes];
@@ -377,7 +252,7 @@ var $skills = array(
 			}
 			if (isset($data['allowableRaces']))
 			{
-				$tt['Attributes']['Race'] = 'Races:';
+				$tt['Attributes']['Race'] = $roster->locale->act['tooltip_races'];
 				foreach($data['allowableRaces'] as $id => $classes)
 				{
 					$tt['Attributes']['RaceText'][] = $roster->locale->act['id_to_race'][$classes];
@@ -473,7 +348,7 @@ var $skills = array(
 			//$tt['Attributes']['MadeBy']['Line'] = $matches[0];
 			if ( $data['heroicTooltip'] )
 			{
-				$tt['Attributes']['Heroic'] = 'Heroic';
+				$tt['Attributes']['Heroic'] = $roster->locale->act['tooltip_heroic'];
 			}
 			if ( isset($data['nameDescription']) )
 			{
@@ -728,7 +603,7 @@ function _getCaption()
 	
 	function _getUpgrade()
 	{
-		$html = "Upgrade Level: " . $this->attributes['Upgrade']['Base'] . "/" . $this->attributes['Upgrade']['Max'] . "<br />";
+		$html = sprintf( $roster->locale->act['tooltip_upgrade'], $this->attributes['Upgrade']['Base'], $this->attributes['Upgrade']['Max']). "<br />";
 		return $html;
 	}
 
@@ -810,7 +685,7 @@ function _getCaption()
 
 	function _getSkillRequired()
 	{
-		$html = 'Requires ' . $this->attributes['SkillRequired'] . "<br />";
+		$html = $roster->locale->act['tooltip_requires'].' ' . $this->attributes['SkillRequired'] . "<br />";
 		return $html;
 	}
 	
--- localization/enUS.php
+++ localization/enUS.php
@@ -896,8 +896,10 @@ $lang['tooltip_damage_reduction']='Damage Reduction';
 $lang['tooltip_durability']='Durability';
 $lang['tooltip_unique']='Unique';
 $lang['tooltip_speed']='Speed';
-$lang['tooltip_poisoneffect']='^Each strike has';
-
+$lang['tooltip_poisoneffect']='Each strike has';
+$lang['tooltip_classes']='Classes:';
+$lang['tooltip_races']='Races:';
+$lang['tooltip_heroic']='Heroic';
 // MOP 5.1
 $lang['tooltip_upgrade']='Upgrade Level: %1$s/%2$s';
 $lang['tooltip_preg_upgrade']='/Upgrade Level: (\d+)\/(\d+)/';
@@ -1358,3 +1360,342 @@ $lang['user_profile_edit'] = array(
 $lang['changeyourpass'] = 'Change Your Password';
 $lang['roster_ucp'] = 'User Control Panel';
 $lang['roster_ucp_ab'] = 'UCP';
+
+$lang['apiitem']['socketbonus']='Socket Bonus: %s';
+$lang['apiitem']['reqlevel']='Requires Level %s';
+$lang['apiitem']['ilevel']='Item Level %s';
+$lang['apiitem']['requires']='Requires %s';
+$lang['apiitem']['dpsspeed']= '%1$s Damage	Speed %2$s';
+//common wording
+$lang['apiitem']['socket']='Socket';
+
+$lang['apiitem']['itemclass'] = array(
+	'0' => 'Consumable',
+	'1' => 'Container',
+	'2' => 'Weapon',
+	'3' => 'Gem',
+	'4' => 'Armor',
+	'5' => 'Reagent',
+	'6' => 'Projectile',
+	'7' => 'Trade Goods',
+	'8' => 'Generic',
+	'9' => 'Book',
+	'10' => 'Money',
+	'11' => 'Quiver',
+	'12' => 'Quest',
+	'13' => 'Key',
+	'14' => 'Permanent',
+	'15' => 'Junk',
+	'16' => 'Glyph'
+);
+
+//itemSubClass
+$lang['apiitem']['itemSubClass'] = array(
+	'0' => array(
+		'0' => 'Consumable',
+		'5' => 'Food & Drink',
+		'1' => 'Potion',
+		'2' => 'Elixir',
+		'3' => 'Flask',
+		'7' => 'Bandage',
+		'6' => 'Item Enhancement',
+		'4' => 'Scroll',
+		'8' => 'Other'
+	),
+	'1' => array('0' => 'Bag',
+		'1' => 'Soul Bag',
+		'2' => 'Herb Bag',
+		'3' => 'Enchanting Bag',
+		'4' => 'Engineering Bag',
+		'5' => 'Gem Bag',
+		'6' => 'Mining Bag',
+		'7' => 'Leatherworking Bag',
+		'8' => 'Inscription Bag',
+		'9' => 'Tackle Box'
+	),
+	'2' => array(
+		'0' => 'Axe',
+		'1' => 'Axe',
+		'2' => 'Bow',
+		'3' => 'Gun',
+		'4' => 'One-Handed Mace',
+		'5' => 'Two-Handed Mace',
+		'6' => 'Polearm',
+		'7' => 'One-Handed Sword',
+		'8' => 'Two-Handed Sword',
+		'9' => 'Obsolete',
+		'10' => 'Stave',
+		'11' => 'One-Handed Exotic',
+		'12' => 'Two-Handed Exotic',
+		'13' => 'Fist Weapon',
+		'14' => 'Miscellaneous',
+		'15' => 'Dagger',
+		'16' => 'Thrown',
+		'17' => 'Spear',
+		'18' => 'Crossbow',
+		'19' => 'Wand',
+		'20' => 'Fishing Pole'
+	),
+	'3' => array(
+		'0' => 'Red',
+		'1' => 'Blue',
+		'2' => 'Yellow',
+		'3' => 'Purple',
+		'4' => 'Green',
+		'5' => 'Orange',
+		'6' => 'Meta',
+		'7' => 'Simple',
+		'8' => 'Prismatic',
+		'9' => 'Hydraulic',
+		'10' => 'Cogwheel'
+	),
+	'4' => array(
+		'0' => 'Miscellaneous',
+		'1' => 'Cloth',
+		'2' => 'Leather',
+		'3' => 'Mail',
+		'4' => 'Plate',
+		'5' => 'Bucklers',
+		'6' => 'Shield',
+		'7' => 'Libram',
+	'8' => 'Idol',
+		'9' => 'Totem',
+		'10' => 'Sigil',
+		'11' => 'Relic'),
+	'5' => array(
+		'0' => 'Reagent'
+	),
+	'6' => array(
+		'0' => 'Wand(OBSOLETE)',
+		'1' => 'Bolt(OBSOLETE)',
+		'2' => 'Arrow',
+		'3' => 'Bullet',
+		'4' => 'Thrown(OBSOLETE)'),
+	'7' => array(
+		'0' => 'Trade Goods',
+		'10' => 'Elemental',
+		'15' => 'Weapon Enchantment - Obsolete',
+		'5' => 'Cloth',
+		'6' => 'Leather',
+		'7' => 'Metal & Stone',
+		'8' => 'Meat',
+		'9' => 'Herb',
+		'12' => 'Enchanting',
+		'4' => 'Jewelcrafting',
+		'1' => 'Parts',
+		'3' => 'Devices',
+		'2' => 'Explosives',
+		'13' => 'Materials',
+		'11' => 'Other',
+		'14' => 'Item Enchantment'
+	),
+	'9' => array(
+		'0' => 'Book',
+		'1' => 'Leatherworking',
+		'2' => 'Tailoring',
+		'3' => 'Engineering',
+		'4' => 'Blacksmithing',
+		'5' => 'Cooking',
+		'6' => 'Alchemy',
+		'7' => 'First Aid',
+		'8' => 'Enchanting',
+		'9' => 'Fishing',
+		'10' => 'Jewelcrafting',
+		'11' => 'Inscription'
+	),
+	'11' => array(
+		'0' => 'Quiver(OBSOLETE)',
+		'1' => 'Quiver(OBSOLETE)',
+		'2' => 'Quiver',
+		'3' => 'Ammo Pouch'
+	),
+	'12' => array(
+		'0' => 'Quest'
+	),
+	'13' => array(
+		'0' => 'Key',
+		'1' => 'Lockpick'
+	),
+	'14' => array(
+		'0' => 'Permanent'
+	),
+	'15' => array(
+		'0' => 'Junk',
+		'1' => 'Reagent',
+		'2' => 'Pet',
+		'3' => 'Holiday',
+		'4' => 'Other',
+		'5' => 'Mount'
+	),
+	'16' => array(
+		'1' => 'Warrior',
+		'2' => 'Paladin',
+		'3' => 'Hunter',
+		'4' => 'Rogue',
+		'5' => 'Priest',
+		'6' => 'Death Knight',
+		'7' => 'Shaman',
+		'8' => 'Mage',
+		'9' => 'Warlock',
+		'11' => 'Druid'
+	)
+);
+
+$lang['apiitem']['slotType'] = array(
+	'0' => 'None',
+	'1' => 'Head',
+	'2' => 'Neck',
+	'3' => 'Shoulder',
+	'4' => 'Shirt',
+	'5' => 'Chest',
+	'6' => 'Waist',
+	'7' => 'Legs',
+	'8' => 'Feet',
+	'9' => 'Wrist',
+	'10' => 'Hands',
+	'11' => 'Finger',
+	'12' => 'Trinket',
+	'13' => 'One-Hand',
+	'14' => 'Shield',
+	'15' => 'Ranged',
+	'16' => 'Cloak',
+	'17' => 'Two-Hand',
+	'18' => 'Bag',
+	'19' => 'Tabard',
+	'20' => 'Robe',
+	'21' => 'Main Hand',
+	'22' => 'Off Hand',
+	'23' => 'Held In Off-hand',
+	'24' => 'Ammo',
+	'25' => 'Thrown',
+	'26' => 'Ranged',
+	'28' => 'Relic'
+);
+
+/*
+stat types
+*/
+$lang['apiitem']['statlocal'] = array(
+	'1' => 'Health',
+	'2' => 'Mana',
+	'3' => 'Agility',
+	'4' => 'Strength',
+	'5' => 'Intellect',
+	'6' => 'Spirit',
+	'7' => 'Stamina',
+	'44' => 'Armor Penetration',
+	'38' => 'Attack Power',
+	'15' => 'Shield Block',
+	'48' => 'the block value of your shield by.',
+	'19' => 'Melee Critical strike',
+	'20' => 'Ranged Critical strike',
+	'32' => 'Critical Strike',
+	'21' => 'Spell Critical strike',
+	'25' => 'Melee Critical avoidance',
+	'26' => 'Ranged Critical avoidance',
+	'34' => 'Critical Avoidance',
+	'27' => 'Spell critical avoidance',
+	'12' => 'Defense',
+	'13' => 'Dodge',
+	'37' => 'Expertise',
+	'40' => 'attack power by in Cat, Bear, Dire Bear, and Moonkin forms only.',
+	'28' => 'Melee Haste',
+	'29' => 'Ranged Haste',
+	'36' => 'Haste',
+	'30' => 'Spell Haste',
+	'16' => 'Melee Hit',
+	'17' => 'Ranged Hit',
+	'31' => 'Hit',
+	'18' => 'Spell Hit',
+	'22' => 'Melee Hit Avoidance',
+	'23' => 'Ranged Hit Avoidance',
+	'33' => 'Hit Avoidance',
+	'24' => 'Spell Hit Avoidance',
+	'43' => 'Equip: Restores mana per 5 sec.',
+	'49' => 'Mastery',
+	'14' => 'Parry',
+	'39' => 'Ranged Attack Power',
+	'35' => 'PvP Resilience',
+	'57' => 'PvP Power',
+	'41' => 'damage done by magical spells and effects by up to.',
+	'42' => 'healing done by magical spells and effects by up to.',
+	'47' => 'Spell Penetration',
+	'45' => 'Spell Power'
+);
+$lang['apiitem']['itemstat'] = array(
+	'1' => '+%s Health',
+	'2' => '+%s Mana',
+	'3' => '+%s Agility',
+	'4' => '+%s Strength',
+	'5' => '+%s Intellect',
+	'6' => '+%s Spirit',
+	'7' => '+%s Stamina',
+	'46' => 'Equip: Restores %s health per 5 sec.',
+	'44' => '+%s Armor Penetration',
+	'38' => '+%s Attack Power',
+	'15' => '+%s Shield Block',
+	'48' => 'the block value of your shield by %s.',
+	'19' => '+%s Melee Critical strike',
+	'20' => '+%s Ranged Critical strike',
+	'32' => '+%s Critical Strike',
+	'21' => '+%s Spell Critical strike',
+	'25' => '+%s Melee Critical avoidance',
+	'26' => '+%s Ranged Critical avoidance',
+	'34' => '+%s Critical Avoidance',
+	'27' => '+%s Spell critical avoidance',
+	//ITEM_MOD_DAMAGE_PER_SECOND_SHORT => 'Damage Per Second',
+	'12' => '+%s Defense',
+	'13' => '+%s Dodge',
+	'37' => '+%s Expertise',
+	'40' => 'attack power by %s in Cat, Bear, Dire Bear, and Moonkin forms only.',
+	'28' => '+%s Melee Haste',
+	'29' => '+%s Ranged Haste',
+	'36' => '+%s Haste',
+	'30' => '+%s Spell Haste',
+	'16' => '+%s Melee Hit',
+	'17' => '+%s Ranged Hit',
+	'31' => '+%s Hit',
+	'18' => '+%s Spell Hit',
+	'22' => '+%s Melee Hit Avoidance',
+	'23' => '+%s Ranged Hit Avoidance',
+	'33' => '+%s Hit Avoidance',
+	'24' => '+%s Spell Hit Avoidance',
+	'43' => 'Equip: Restores %s mana per 5 sec.',
+	'49' => '+%s Mastery',
+	'14' => '+%s Parry',
+	'39' => '+%s Ranged Attack Power',
+	'35' => '+%s PvP Resilience',
+	'57' => '+%s PvP Power',
+	'41' => 'damage done by magical spells and effects by up to %s.',
+	'42' => 'healing done by magical spells and effects by up to %s.',
+	'47' => '+%s Spell Penetration',
+	'45' => '+%s Spell Power'
+);
+
+$lang['apiitem']['bind'] = array(
+	'0' => '',
+	'8' => "Account Bound",
+	'2' => "Binds when equipped",
+	'1' => "Binds when picked up",
+	'3' => "Binds when used",
+	'4' => "Quest Item",
+	'5' => "Binds to account",
+	'6' => "Binds to Battle.net account",
+	'7' => "Battle.net Account Bound"
+);
+
+$lang['apiitem']['skills'] = array(
+	"185" => "Cooking",
+	"773" => "Inscription",
+	"755" => "Jewelcrafting",
+	"393" => "Skinning",
+	"333" => "Enchanting",
+	"202" => "Engineering",
+	"197" => "Tailoring",
+	"186" => "Mining",
+	"182" => "Herbalism",
+	"171" => "Alchemy",
+	"165" => "Leatherworking",
+	"164" => "Blacksmithing"
+);
+
